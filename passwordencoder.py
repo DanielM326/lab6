906194d3b49050bfa05ab3cd3b69ba72fba8b7cd
DanielM326 <moodyd@ufl.edu>
2024-03-11 18:36:08 -0400

    "Created password encoder."

---
+++ passwordencoder.py
@@ -0,0 +1,35 @@
+# Daniel Moody Lab 6
+
+def encode(password):
+    encode_pass = ""
+
+    for i in password:
+        digit = int(i) + 3
+        encode_pass += str(digit)[-1]
+
+    return encode_pass
+
+
+if __name__ == '__main__':
+
+    while True:
+
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        print()
+
+        user_option = int(input("Please enter an option: "))
+
+        if user_option == 1:
+            user_password = input("Please enter your password to encode: ")
+            pass_encoded = encode(user_password)
+
+            print("Your password has been encoded and stored!")
+            print()
+        elif user_option == 2:
+            pass
+        elif user_option == 3:
+            break
